9641ab33bf3ce66e02e0687c34f4f809bffbd808
cecilia-89 <catabong89@gmail.com>
2023-01-11 21:34:21 +0100

    Modified file

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 -- Creates table users if it doesn't exits don't throw error
 CREATE TABLE Users (
   id int NOT NULL,
-  email varchar(255) NOT NULL UNIQUE,
+  email varchar(255) NOT NULL,
   name varchar(255),
   PRIMARY KEY(id)
 ) IF NOT EXISTS;
