493c068a0a5b1e0e83f82b3a2b1b7e466850f1c8
cecilia-89 <catabong89@gmail.com>
2023-01-17 12:56:25 +0100

    switched to correct method

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,5 +4,5 @@
 
 def schools_by_topic(mongo_collection, topic):
     """returns the list of school having a specific topic"""
-    query = mongo_collection.find({"topic": topic})
+    query = mongo_collection.find({"topics": topic})
     return query
