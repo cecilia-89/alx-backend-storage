fcc3505aac503c944956490cf076cc02dfddce91
cecilia-89 <catabong89@gmail.com>
2023-01-17 11:55:06 +0100

    switched to deprecated methods

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -3,5 +3,6 @@
 
 
 def schools_by_topic(mongo_collection, topic):
+    """returns the list of school having a specific topic"""
     query = mongo_collection.find({"topic": topic})
     return query
