7234642bd146dbfe0086de3998b0a040b0440477
cecilia-89 <catabong89@gmail.com>
2023-01-17 10:15:22 +0100

    minor modifications

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -2,7 +2,7 @@
 """Module: 11-schools_by_topic"""
 
 
-if __name__ == '__main__':
-    def schools_by_topic(mongo_collection, topic):
-        query = mongo_collection.find({"topic": topic})
-        return query
+
+def schools_by_topic(mongo_collection, topic):
+    query = mongo_collection.find({"topic": topic})
+    return query
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -2,8 +2,7 @@
 """Module: 9-insert"""
 
 
-if __name__ == '__main__':
-    def insert_school(mongo_collection, **kwargs):
-        """inserts a new document in a collection"""
-        result = mongo_collection.insert_one(kwargs)
-        return result.inserted_id
+def insert_school(mongo_collection, **kwargs):
+    """inserts a new document in a collection"""
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
