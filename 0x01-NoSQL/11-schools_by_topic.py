fa5c3127951bd676b9610c2fd2ae0d1eb7736be6
cecilia-89 <catabong89@gmail.com>
2023-01-17 11:15:54 +0100

    minor modifications

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """Module: 11-schools_by_topic"""
 
-
-
 def schools_by_topic(mongo_collection, topic):
     query = mongo_collection.find({"topic": topic})
     return query
--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,18 +2,18 @@
 """Module: 12-log stats"""
 from pymongo import MongoClient
 
+if __name__ == "__main__":
+    db = MongoClient().logs
+    collection = db.nginx
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    print("{} logs\nMethods:".format(collection.count_documents({})))
+    for method in methods:
+        print("\tmethod {}: {}".format(
+            method,
+            len(list(collection.find({"method": method})))
+        ))
 
-db = MongoClient().logs
-collection = db.nginx
-methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-print("{} logs\nMethods:".format(collection.count_documents({})))
-for method in methods:
-    print("\tmethod {}: {}".format(
-        method,
-        len(list(collection.find({"method": method})))
-    ))
-
-print("{} status check".format(len(list(collection.aggregate([
-    {"$match": {"method": "GET"}},
-    {"$match": {"path": "/status"}}]
-    )))))
+    print("{} status check".format(len(list(collection.aggregate([
+        {"$match": {"method": "GET"}},
+        {"$match": {"path": "/status"}}]
+        )))))
