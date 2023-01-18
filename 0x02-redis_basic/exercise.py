d6eb53cb8964ba2268b91100790d73c474125e4f
cecilia-89 <catabong89@gmail.com>
2023-01-18 20:14:19 +0100

    corrected type annotations

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 '''Writing strings to Redis'''
 import redis
 import uuid
-from typing import Callable, Any
+from typing import Callable, Union
 
 
 class Cache:
@@ -13,7 +13,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """stores the input in redis"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
