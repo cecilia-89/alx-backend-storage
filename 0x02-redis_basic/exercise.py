8fcbff057b6358473eb9637f736482dcc2b3e737
cecilia-89 <catabong89@gmail.com>
2023-01-18 19:52:57 +0100

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,7 +13,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: any) -> str:
+    def store(self, data: int) -> str:
         """stores the input in redis"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
